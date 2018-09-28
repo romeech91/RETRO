46257ecd816c5f458010199acbcbd740ee4e57a4
romeech91 <romontino@gmail.com>
2018-09-28 13:54:22 +0300

    filter by tag 90%

--- src/assets/scripts/portfolio.js
+++ src/assets/scripts/portfolio.js
@@ -8,9 +8,26 @@ function createItems(item) {
     list.classList.add('gallery__list');
 
     item.forEach((element, index) => {
+        const filterTag = document.querySelector('.portfolio-filter');
+
+        filterTag.addEventListener('click', (e) => {
+            li.classList.remove('visually-hidden');
+            let liTag = li.getAttribute('data-tag');
+            console.log(liTag);
+            let currentTag = e.target.dataset.tag;
+            
+            li.style.display = "none";
+            if (currentTag === liTag) {
+                li.style.display = "flex";
+            }
+            liTag.forEach(elem => {
+                
+            })
+        })
 
         let li = document.createElement('li');
         li.classList.add('gallery__item');
+        li.setAttribute("data-tag", element.tags);
         li.style.backgroundImage = "url(" + element.imageURL + ")";
         let block = document.createElement('div');
         block.classList.add('bottom-block');
@@ -32,6 +49,9 @@ function createItems(item) {
         block.appendChild(circle);
         circle.appendChild(circleImg);
 
+        
+
+
         if (index > 7) {
             li.classList.add('visually-hidden');
         }
@@ -46,26 +66,31 @@ function createItems(item) {
     });
     
     gallery.appendChild(list);
-    console.log(list);
+    
 }
 
 createItems(data);
 
 const container = document.querySelector('.portfolio');
 const close = document.querySelector('.close-btn');
+const itemsGal = document.querySelectorAll('.gallery__item');
 
-gallery.addEventListener('click', function(e) {
-    var path = e.target.style.backgroundImage;
-    var bigWindow = document.createElement('div');
-    bigWindow.classList.add('big-image');
-    var close = document.createElement('div');
-    close.classList.add('close-btn');
-    bigWindow.appendChild(close);
-    container.appendChild(bigWindow);
-    bigWindow.style.backgroundImage = path;
+itemsGal.forEach(element => {
+    element.addEventListener('click', function(e) {
+        var path = e.target.style.backgroundImage;
 
-    close.addEventListener('click', function() {
-        container.removeChild(bigWindow);
+        var bigWindow = document.createElement('div');
+        bigWindow.classList.add('big-image');
+        var close = document.createElement('div');
+        close.classList.add('close-btn');
+        bigWindow.appendChild(close);
+        container.appendChild(bigWindow);
+        bigWindow.style.backgroundImage = path;
+        
+        close.addEventListener('click', function () {
+            container.removeChild(bigWindow);
+        })
     })
-
 });
+
+
